bb5069dcbc076db74e6907945050454c89183866
LUIS-H <luisbv.hdez28@gmail.com>
2020-10-08 07:10:49 -0500

    1.1

---
+++ src/main/java/VentanaPrueba.java
@@ -0,0 +1,15 @@
+/**
+ * Hola
+ * @author Luis
+ */
+import javax.swing.*;
+
+public class VentanaPrueba {
+    
+    public static void main(String[] args) {
+        JFrame jf = new JFrame("Mi Ventana");
+        jf..setSize(300,400);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setVisible(true);
+    }    
+}
